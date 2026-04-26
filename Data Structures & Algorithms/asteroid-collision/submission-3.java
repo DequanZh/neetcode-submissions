d590706c974748d562db7afd610948a65da7bc79
DequanZh <114198006+DequanZh@users.noreply.github.com>
2026-04-25 18:52:51 -0700

    Add: asteroid-collision - submission-3

---
+++ Data Structures & Algorithms/asteroid-collision/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new LinkedList<>();
+        for(int asteroid : asteroids){
+            if(asteroid > 0 || stack.size() == 0){
+                stack.addLast(asteroid);
+                continue;
+            }
+            while(stack.size() > 0 && stack.peekLast() < Math.abs(asteroid) && stack.peekLast() > 0){
+                stack.removeLast();
+            }
+            if(stack.size() > 0 && stack.peekLast() >= Math.abs(asteroid) && stack.peekLast() > 0){
+                if(stack.peekLast() == Math.abs(asteroid)){
+                    stack.removeLast();
+                }
+                continue;
+            }
+            stack.addLast(asteroid);
+        }
+        int[] stackArray = new int[stack.size()];
+        int j = 0;
+        while(stack.size() > 0){
+            stackArray[j] = stack.removeFirst();
+            j++;
+        }
+        return stackArray;
+    }
+}
