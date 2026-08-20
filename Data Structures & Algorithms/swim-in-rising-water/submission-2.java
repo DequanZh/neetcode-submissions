f24de013d86c9f836d3baacc339b62c3feac06dd
DequanZh <114198006+DequanZh@users.noreply.github.com>
2026-08-19 21:45:17 -0700

    Add: swim-in-rising-water - submission-2

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int swimInWater(int[][] grid) {
+        int n = grid.length;
+        boolean[][] visted = new boolean[n][n];
+        //[i,j,time]
+        PriorityQueue<int[]> pq = new PriorityQueue<>((p1,p2)->{
+            return p1[2] - p2[2];
+        });
+        pq.offer(new int[]{0,0,grid[0][0]});
+        int[][] nextDir = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+        visted[0][0] = true;
+        while(!pq.isEmpty()){
+            int[] curPair = pq.poll();
+            int x = curPair[0], y = curPair[1], time = curPair[2];
+            if(x == n-1 && y == n-1){
+                return time;
+            }
+            for(int[] next : nextDir){
+                int dx = x + next[0], dy = y + next[1];
+                if(dx >= 0 && dx < n && dy >= 0 && dy < n && !visted[dx][dy]){
+                    visted[dx][dy] = true;
+                    pq.offer(new int[]{
+                        dx,
+                        dy,
+                        Math.max(time,grid[dx][dy])
+                    });
+                }
+            }
+        }
+        return -1;
+    }
+}
